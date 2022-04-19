cf9ae7289d46349b63ee99beb2647dcc965c9e52
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-19 21:40:32 +0000

    Changed result of calculations to integer

--- convert.php
+++ convert.php
@@ -3,6 +3,6 @@
 <?php
   $celsius = floatval($_POST['temp']);
   $fahrenheit = 9 / 5 * $celsius + 32;
-  echo "The temperature in Fahrenheit is " . number_format($fahrenheit, 2) . " degrees Fahrenheit.";
+  echo "The temperature in Fahrenheit is " . (int)$fahrenheit . " degrees Fahrenheit.";
 ?>
 </html>
